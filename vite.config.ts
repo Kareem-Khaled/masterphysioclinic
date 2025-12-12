1c2f82cb3f1d6d183a9e897989654cff493a2f94
Kareem-Khaled <kareemkhaled143.com>
2025-12-12 14:54:50 +0200

    Add gh-pages deployment script and update package dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Skilled_Healer_Clinic/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
